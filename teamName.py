3e8873ba62d17f927bb74c8cd877c3bb48b6275e
Will-H007 <wkayi076@gmail.com>
2022-07-10 14:55:54 +0800

    update

--- teamName.py
+++ teamName.py
@@ -5,18 +5,25 @@ import matplotlib.pyplot as plt
 nInst=100
 currentPos = np.zeros(nInst)
 
+def sell(df):
+    df = np.zeros(nInst)
+    return df
 
 #Return a list showing the average percentage change over a period from highest to lowest
 def buy_list(df, budget, start_date, end_date, concentration = 10):
     percentage_change = df[start_date:end_date].pct_change()
+    # print(df[start_date:end_date].pct_change(period = 30))
     average_percentage_change = percentage_change.mean()
+    # plt.plot(average_percentage_change)
+    # plt.show()
     average_percentage_change.name = "Buylist"
     # print(average_percentage_change)
     average_percentage_change = average_percentage_change.sort_values(ascending=False)
+
     # x_axis = average_percentage_change.keys()
     # print(x_axis)
-    plt.bar(average_percentage_change.keys(),average_percentage_change.values)
-    plt.show()
+    # plt.bar(average_percentage_change.keys(),average_percentage_change.values)
+    # plt.show()
     if(len(average_percentage_change)>= 10):
         average_percentage_change = average_percentage_change.sort_values(ascending=False)[:concentration]
     else:
@@ -31,8 +38,16 @@ def getMyPosition (prcSoFar):
     global currentPos
     if len(prcSoFar[0]) % 30 == 0:
         df = pd.DataFrame(prcSoFar).transpose()
-        buy_list(df = df, budget = 100, start_date = len(prcSoFar[0]) - 30, end_date = len(prcSoFar[0]), concentration = 3)
-    # print(prcSoFar)
+        currentPos = sell(df)
+
+        buy = buy_list(df = df, budget = 10
+                 , start_date = len(prcSoFar[0]) - 30, end_date = len(prcSoFar[0]), concentration = 15)
+        stocks_to_buy = np.array(buy.keys())
+        money_to_spend = np.array(buy.values)
+        for i in range(len(stocks_to_buy)):
+            currentPos[stocks_to_buy[i]] += money_to_spend[i]
+
+        print(currentPos)
     # Build your function bod
     # y here
 
